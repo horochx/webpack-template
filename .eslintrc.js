118d3732d81cc05892d6534524a0ce243c166b8e
horochx <horochx@gmail.com>
2019-04-20 11:48:03 +0800

    add eslint and lint-staged

---
+++ .eslintrc.js
@@ -0,0 +1,69 @@
+module.exports = {
+  root: true,
+
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+  },
+
+  env: {
+    browser: true,
+    node: true,
+  },
+
+  globals: {},
+
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+
+  plugins: ['react-hooks'],
+
+  rules: {
+    // base
+    'no-magic-numbers': 'warn',
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'eol-last': ['error', 'always'],
+    'jsx-quotes': ['error', 'prefer-double'],
+
+    // typescript
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: false,
+        },
+      },
+    ],
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+
+    // react
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+}
--- src/declareReact.d.ts
+++ src/declareReact.d.ts
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react' // eslint-disable-line
 
 declare module 'react' {
   interface HTMLAttributes<T> {
